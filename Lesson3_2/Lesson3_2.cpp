503d546c9fbdafc5d09f861aa2aa074d65a0ff54
Ness <bur>
2024-09-05 18:15:42 +0300

    Добавьте файлы проекта.

---
+++ Lesson3_2/Lesson3_2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int firstNumber;
+    int secondNumber;
+    int sum;
+    cout << "Введите первое число: ";
+      cin >> firstNumber;
+      cout << "Введите второе число: ";
+      cin >> secondNumber;
+      cout << "Введите их сумму: ";
+      cin >> sum;
+    cout << "-----Проверяем----- \n";
+    if (firstNumber + secondNumber == sum) {
+        cout << "Верно! Забери пирожок в ванной ";
+    }
+    else {
+        cout << "Ошибка! Поэтому пирожок съем я \n";
+        cout << "Верный ответ:" << firstNumber + secondNumber;
+    }
+}
+
